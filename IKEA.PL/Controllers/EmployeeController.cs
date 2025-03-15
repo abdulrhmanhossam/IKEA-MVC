89ea68ad279242a215920310e78754df84f6f34c
abdulrahman <abdulrahmn398@outlook.com>
2025-03-15 20:30:18 +0200

    Add Employee management features
    - Introduced navigation link for "Employee" in `_Layout.cshtml`.
    - Updated `_ViewImports.cshtml` to include `IKEA.BLL.Models.Employees`.
    - Created `EmployeeController` with dependency injection and an `Index` action to list employees.
    - Added `Index.cshtml` view for displaying employee details with options to view, edit, and delete, including a confirmation modal for deletions.

---
+++ IKEA.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using IKEA.BLL.Services.Employees;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKEA.PL.Controllers;
+
+public class EmployeeController : Controller
+{
+    private readonly IEmployeeService _employeeService;
+    private readonly IWebHostEnvironment _environment;
+    private readonly ILogger<EmployeeController> _logger;
+
+    public EmployeeController
+        (IEmployeeService employeeService,
+        IWebHostEnvironment environment, ILogger<EmployeeController> logger)
+    {
+        _employeeService = employeeService;
+        _environment = environment;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var employees = _employeeService.GetAllEmployees();
+
+        return View(employees);
+    }
+}
